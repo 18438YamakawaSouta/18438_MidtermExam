accb108df5481ecea883ee7a405481aca278510e
SoutaY <18438@toba-cmt.ac.jp>
2021-11-17 15:08:05 +0900

    コードを作成

--- tcp_s..py
+++ tcp_s..py
@@ -1,7 +1,7 @@
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sOcKettt:
-    sOcKettt.bind(('0.0.0.0',50007))
+    sOcKettt.bind(('127.0.0.1',50007))
     sOcKettt.listen(1)
     while True:
         connECtion, FROMaddr = sOcKettt.accept()
